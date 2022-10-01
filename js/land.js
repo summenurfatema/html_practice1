bc9d000782bb08f81e5b2df2a87fc5d47912453c
Fatema04 <shadrinmoni15@gmail.com>
2022-09-30 19:28:14 -0700

    25% completed

--- js/land.js
+++ js/land.js
@@ -6,7 +6,7 @@ const showData = (cards) => {
     const cardContainer = document.getElementById('card')
     for (let card of cards) {
         const cardDiv = document.createElement('div')
-        cardDiv.className = ('p-5', 'rounded-3xl', 'border-2')
+        cardDiv.className = ('p-5', 'border-2')
         cardDiv.innerHTML = `
         <img class="h-56 p-3 rounded-3xl" src=${card.img} alt=""/>
         <h1 class="text-3xl pb-4 font-semibold text-black text-center">${card.names}</h1>
